a0cc2477543f66798226b9c12112e02c77b39980
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-04-29 22:05:45 -0500

    project GDU - admintle - teach

---
+++ resources/views/base.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Pagina Principal')
+
+{{-- @section('content_header')
+    <h1 class="text-center font-weight-bold text-uppercase">Gestión de Tipo de Practica</h1>
+@stop --}}
+
+@section('css')
+<link rel="stylesheet" href="hola.css">
+@stop
+
+@section('content')
+
+@stop
+
+
+
+@section('js')
+<script src="hola.js"></script>
+@stop
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Pagina Principal')
+
+{{-- @section('content_header')
+    <h1 class="text-center font-weight-bold text-uppercase">Gestión de Tipo de Practica</h1>
+@stop --}}
+
+@section('css')
+<link rel="stylesheet" href="hola.css">
+@stop
+
+@section('content')
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit animi, eum officiis fugit error ex voluptate libero illum dolore a similique amet mollitia soluta commodi aspernatur tempora, rerum, voluptas provident.
+    Temporibus illum iste omnis sint eos qui, minus quae reprehenderit magnam.
+</p>
+@stop
+
+
+
+@section('js')
+<script src="hola.js"></script>
+@stop
